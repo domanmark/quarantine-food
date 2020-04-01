de7ce880b9a6a61ad788b7727670f5bd960e59a0
domanmark <mpd.github@gmail.com>
2020-03-31 20:02:16 -0400

    Updates

---
+++ precache-manifest.0f75f204c928c1ff885ab46f34325ec1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92db7cc960d58c11c59cdbcd6015997",
+    "url": "/quarantine-food/index.html"
+  },
+  {
+    "revision": "f5ebb805b39d6e7560db",
+    "url": "/quarantine-food/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "16bd0982c5e5329c252c",
+    "url": "/quarantine-food/static/css/main.176b7693.chunk.css"
+  },
+  {
+    "revision": "f5ebb805b39d6e7560db",
+    "url": "/quarantine-food/static/js/2.553ff52f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quarantine-food/static/js/2.553ff52f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16bd0982c5e5329c252c",
+    "url": "/quarantine-food/static/js/main.43cb2a97.chunk.js"
+  },
+  {
+    "revision": "f83e859260ac9d155b22",
+    "url": "/quarantine-food/static/js/runtime-main.0dfcb8cf.js"
+  },
+  {
+    "revision": "b620f02b4440309326469cbb0d1b45d3",
+    "url": "/quarantine-food/static/media/logo.b620f02b.png"
+  }
+]);
